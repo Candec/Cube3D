1e83f738d8ec90b55baf47db176c1ee9440c0c2e
candec <omnius.candec@gmail.com>
2023-01-13 20:23:34 +0000

    sending

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: jibanez- <jibanez- <jibanez-@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:10:54 by jibanez-          #+#    #+#             */
-/*   Updated: 2023/01/10 14:51:18 by jibanez-         ###   ########.fr       */
+/*   Updated: 2023/01/13 18:21:01 by jibanez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	add_pixel(t_img *frame, int rgb, int x, int y)
 {
-	frame->data[y * frame->size_l + x] = rgb;
+	frame->data[y * WIDTH + x] = rgb;
 }
 
 void	draw_bg(t_mlx *cube)
@@ -22,13 +22,13 @@ void	draw_bg(t_mlx *cube)
 	int x;
 	int y;
 
-	x = -1;
 	y = -1;
 	while (++y < HEIGHT)
 	{
+		x = -1;
 		while (++x < WIDTH)
 		{
-			if (y > HEIGHT / 2)
+			if (y > (HEIGHT / 2))
 				add_pixel(&cube->frame, cube->map.c_rgb.int_rgb, x, y);
 			else
 				add_pixel(&cube->frame, cube->map.f_rgb.int_rgb, x, y);
